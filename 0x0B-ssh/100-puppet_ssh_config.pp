1284459b8e5453fc5ad77156acbd1470393afd89
Nwaigba1234567 <iamkatty3@gmail.com>
2023-08-27 17:32:58 -0700

    Added ssh rsa key and public key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,13 @@
 
 include stdlib
 
-file { 'Refuse to authenticate using a password':
+  file { 'Refuse to authenticate using a password':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
-file { 'Use private key':
+  file { 'Use private key':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school'
